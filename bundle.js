7073d520463b153c44330f6a27f6ee1a9c895fa9
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2016-01-31 21:58:51 +0100

    demo

---
+++ bundle.js
@@ -0,0 +1,358 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	const fish = __webpack_require__(1);
+	const beaker = __webpack_require__(4);
+
+	document.body.appendChild(createSprite(fish));
+	document.body.appendChild(createSprite(beaker));
+
+	/**
+	 * <svg>
+	 *   <use xlink:href="#sprite-id"></use>
+	 * </svg>
+	 */
+	function createSprite (glyph) {
+	  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+	  svg.classList.add("icon");
+
+	  const use = document.createElementNS("http://www.w3.org/2000/svg", "use");
+	  use.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", glyph);
+	  svg.appendChild(use);
+
+	  return svg;
+	}
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	;
+	var sprite = __webpack_require__(2);;
+	var image = "<symbol viewBox=\"0 26 100 48\" id=\"fish\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"> <path d=\"M98.5,47.5C96.5,45.5,81,35,62,35c-2.1,0-4.2,0.1-6.2,0.3L39,26c0,4.5,1.3,9,2.4,12.1C31.7,40.7,23.3,44,16,44L0,34\n        c0,8,4,16,4,16s-4,8-4,16l16-10c7.3,0,15.7,3.3,25.4,5.9C40.3,65,39,69.5,39,74l16.8-9.3c2,0.2,4.1,0.3,6.2,0.3\n            c19,0,34.5-10.5,36.5-12.5S100.5,49.5,98.5,47.5z M85.5,50c-1.4,0-2.5-1.1-2.5-2.5s1.1-2.5,2.5-2.5s2.5,1.1,2.5,2.5\n                C88,48.9,86.9,50,85.5,50z\"/> </symbol>";
+	module.exports = sprite.add(image, "fish");
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var Sprite = __webpack_require__(3);
+	var globalSprite = new Sprite();
+
+	if (document.body) {
+	  globalSprite.elem = globalSprite.render(document.body);
+	} else {
+	  document.addEventListener('DOMContentLoaded', function () {
+	    globalSprite.elem = globalSprite.render(document.body);
+	  }, false);
+	}
+
+	module.exports = globalSprite;
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	/**
+	 * List of SVG attributes to fix url target in them
+	 * @type {string[]}
+	 */
+	var fixAttributes = [
+	  'clipPath',
+	  'colorProfile',
+	  'src',
+	  'cursor',
+	  'fill',
+	  'filter',
+	  'marker',
+	  'markerStart',
+	  'markerMid',
+	  'markerEnd',
+	  'mask',
+	  'stroke'
+	];
+
+	/**
+	 * Query to find'em
+	 * @type {string}
+	 */
+	var fixAttributesQuery = '[' + fixAttributes.join('],[') + ']';
+	/**
+	 * @type {RegExp}
+	 */
+	var URI_FUNC_REGEX = /^url\((.*)\)$/;
+
+	/**
+	 * Convert array-like to array
+	 * @param {Object} arrayLike
+	 * @returns {Array.<*>}
+	 */
+	function arrayFrom(arrayLike) {
+	  return Array.prototype.slice.call(arrayLike, 0);
+	}
+
+	/**
+	 * Handles forbidden symbols which cannot be directly used inside attributes with url(...) content.
+	 * Adds leading slash for the brackets
+	 * @param {string} url
+	 * @return {string} encoded url
+	 */
+	function encodeUrlForEmbedding(url) {
+	  return url.replace(/\(|\)/g, "\\$&");
+	}
+
+	/**
+	 * Replaces prefix in `url()` functions
+	 * @param {Element} svg
+	 * @param {string} currentUrlPrefix
+	 * @param {string} newUrlPrefix
+	 */
+	function baseUrlWorkAround(svg, currentUrlPrefix, newUrlPrefix) {
+	  var nodes = svg.querySelectorAll(fixAttributesQuery);
+
+	  if (!nodes) {
+	    return;
+	  }
+
+	  arrayFrom(nodes).forEach(function (node) {
+	    if (!node.attributes) {
+	      return;
+	    }
+
+	    arrayFrom(node.attributes).forEach(function (attribute) {
+	      if (fixAttributes.indexOf(attribute.localName) !== -1) {
+	        var match = URI_FUNC_REGEX.exec(node.getAttribute(attribute.localName));
+
+	        // Do not touch urls with unexpected prefix
+	        if (match && match[1].indexOf(currentUrlPrefix) === 0) {
+	          var referenceUrl = encodeUrlForEmbedding(newUrlPrefix + match[1].split(currentUrlPrefix)[1]);
+	          node.setAttribute(attribute.localName, 'url(' + referenceUrl + ')');
+	        }
+	      }
+	    });
+	  });
+	}
+
+	/**
+	 * Because of Firefox bug #353575 gradients and patterns don't work if they are within a symbol.
+	 * To workaround this we move the gradient definition outside the symbol element
+	 * @see https://bugzilla.mozilla.org/show_bug.cgi?id=353575
+	 * @param {Element} svg
+	 */
+	var FirefoxSymbolBugWorkaround = function (svg) {
+	  var defs = svg.querySelector('defs');
+
+	  var moveToDefsElems = svg.querySelectorAll('symbol linearGradient, symbol radialGradient, symbol pattern');
+	  for (var i = 0, len = moveToDefsElems.length; i < len; i++) {
+	    defs.appendChild(moveToDefsElems[i]);
+	  }
+	};
+
+	/**
+	 * @type {string}
+	 */
+	var DEFAULT_URI_PREFIX = '#';
+
+	/**
+	 * @type {string}
+	 */
+	var xLinkHref = 'xlink:href';
+	/**
+	 * @type {string}
+	 */
+	var xLinkNS = 'http://www.w3.org/1999/xlink';
+	/**
+	 * @type {string}
+	 */
+	var svgOpening = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="' + xLinkNS + '"';
+	/**
+	 * @type {string}
+	 */
+	var svgClosing = '</svg>';
+	/**
+	 * @type {string}
+	 */
+	var contentPlaceHolder = '{content}';
+
+	/**
+	 * Representation of SVG sprite
+	 * @constructor
+	 */
+	function Sprite() {
+	  var baseElement = document.getElementsByTagName('base')[0];
+	  var currentUrl = window.location.href.split('#')[0];
+	  var baseUrl = baseElement && baseElement.href;
+	  this.urlPrefix = baseUrl && baseUrl !== currentUrl ? currentUrl + DEFAULT_URI_PREFIX : DEFAULT_URI_PREFIX;
+
+	  this.content = [];
+	  this.isFirefox = /firefox\//i.test(navigator.userAgent);
+	  this.isIE = /(trident|edge)\//i.test(navigator.userAgent);
+
+	  if (!this.isIE && baseUrl) {
+	    window.addEventListener('spriteLoaderLocationUpdated', function (e) {
+	      var currentPrefix = this.urlPrefix;
+	      var newUrlPrefix = e.detail.newUrl.split(DEFAULT_URI_PREFIX)[0] + DEFAULT_URI_PREFIX;
+	      baseUrlWorkAround(this.svg, currentPrefix, newUrlPrefix);
+	      this.urlPrefix = newUrlPrefix;
+
+	      if (this.isFirefox) {
+	        var nodes = arrayFrom(document.querySelectorAll('use[*|href]'));
+	        nodes.forEach(function (node) {
+	          var href = node.getAttribute(xLinkHref);
+	          if (href && href.indexOf(currentPrefix) === 0) {
+	            node.setAttributeNS(xLinkNS, xLinkHref, newUrlPrefix + href.split(DEFAULT_URI_PREFIX)[1]);
+	          }
+	        });
+	      }
+	    }.bind(this));
+	  }
+	}
+
+	Sprite.styles = ['position:absolute', 'width:0', 'height:0', 'visibility:hidden'];
+
+	Sprite.spriteTemplate = svgOpening + ' style="'+ Sprite.styles.join(';') +'"><defs>' + contentPlaceHolder + '</defs>' + svgClosing;
+	Sprite.symbolTemplate = svgOpening + '>' + contentPlaceHolder + svgClosing;
+
+	/**
+	 * @type {Array<String>}
+	 */
+	Sprite.prototype.content = null;
+
+	/**
+	 * @param {String} content
+	 * @param {String} id
+	 */
+	Sprite.prototype.add = function (content, id) {
+	  if (this.svg) {
+	    this.appendSymbol(content);
+	  }
+
+	  this.content.push(content);
+
+	  return DEFAULT_URI_PREFIX + id;
+	};
+
+	/**
+	 *
+	 * @param content
+	 * @param template
+	 * @returns {Element}
+	 */
+	Sprite.prototype.wrapSVG = function (content, template) {
+	  var svgString = template.replace(contentPlaceHolder, content);
+
+	  var svg = new DOMParser().parseFromString(svgString, 'image/svg+xml').documentElement;
+
+	  if (!this.isIE && this.urlPrefix) {
+	    baseUrlWorkAround(svg, DEFAULT_URI_PREFIX, this.urlPrefix);
+	  }
+
+	  return svg;
+	};
+
+	Sprite.prototype.appendSymbol = function (content) {
+	  var symbol = this.wrapSVG(content, Sprite.symbolTemplate).childNodes[0];
+
+	  this.svg.querySelector('defs').appendChild(symbol);
+	  if (this.isFirefox) {
+	    FirefoxSymbolBugWorkaround(this.svg);
+	  }
+	};
+
+	/**
+	 * @returns {String}
+	 */
+	Sprite.prototype.toString = function () {
+	  var wrapper = document.createElement('div');
+	  wrapper.appendChild(this.render());
+	  return wrapper.innerHTML;
+	};
+
+	/**
+	 * @param {HTMLElement} [target]
+	 * @param {Boolean} [prepend=true]
+	 * @returns {HTMLElement} Rendered sprite node
+	 */
+	Sprite.prototype.render = function (target, prepend) {
+	  target = target || null;
+	  prepend = typeof prepend === 'boolean' ? prepend : true;
+
+	  var svg = this.wrapSVG(this.content.join(''), Sprite.spriteTemplate);
+
+	  if (this.isFirefox) {
+	    FirefoxSymbolBugWorkaround(svg);
+	  }
+
+	  if (target) {
+	    if (prepend && target.childNodes[0]) {
+	      target.insertBefore(svg, target.childNodes[0]);
+	    } else {
+	      target.appendChild(svg);
+	    }
+	  }
+
+	  this.svg = svg;
+
+	  return svg;
+	};
+
+	module.exports = Sprite;
+
+
+/***/ },
+/* 4 */
+/***/ function(module, exports, __webpack_require__) {
+
+	;
+	var sprite = __webpack_require__(2);;
+	var image = "<symbol viewBox=\"214.7 0 182.6 792\" id=\"beaker\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"> <circle cx=\"344.8\" cy=\"20.2\" r=\"20.2\"/> <circle cx=\"344.8\" cy=\"92.9\" r=\"20.2\"/> <circle cx=\"320.5\" cy=\"169.7\" r=\"24.2\"/> <circle cx=\"243\" cy=\"141.4\" r=\"24.2\"/> <circle cx=\"284.2\" cy=\"56.6\" r=\"36.4\"/> <path d=\"M397.3 768.5l-11.3-343.5v-178.5l9.7-20.2h-29.9l-4 13.7 2.4 189.9 9.7 316.8c1.6 14.5-19.4 12.9-34.8 12.9h-33.9s-38.8.8-46.1 0c-14.5-.8-18.6 1.6-17-29.1 1.6-25.2.8-95.5 5.7-301.5l-2.4-189.9-3.2-12.9h-27.5l9.7 20.2v178.6s-3.2 323.3-3.2 343.5c0 26.7 22.6 23.4 22.6 23.4h128.5c7.2 0 25 0 25-23.4zM259.9 473.5h93.8l-1.6-79.9h-92.2zM356.1 611.7l-2.4-132.6v5.7h-93.8l-.8 126.9zM352.1 382.3l-1.7-99.4h-89.7l-.8 99.4zM259.1 623l-.8 123.6h101l-2.4-123.6z\"/> </symbol>";
+	module.exports = sprite.add(image, "beaker");
+
+/***/ }
+/******/ ]);
